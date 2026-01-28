e7296beb56574ed1cf44215510a985107ef7ba3a
Ahmad <134114334+AhmadXDev@users.noreply.github.com>
2026-01-28 15:09:22 +0300

    first commit

---
+++ src/main/java/com/elm/learning2/controller/TaskController.java
@@ -0,0 +1,16 @@
+package com.elm.learning2.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@ResetController
+public class TaskController {
+
+    @GetMapping("/tasks")
+    public
+
+
+
+
+}
